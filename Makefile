d858b8d5ab53fe11157f6beca8e32fa5fc9646b6
raccoon <raccoonandnin@mail.ru>
2026-08-14 19:05:29 +0300

    add tests and test coverage

--- Makefile
+++ Makefile
@@ -1,8 +1,19 @@
+COVERAGE_MIN=80
+
 install:
 	composer install
 
 lint:
-	composer exec --verbose phpcs -- --standard=PSR12 src bin
+	composer exec --verbose phpcs -- --standard=PSR12 src bin tests
 
 test:
 	composer exec --verbose phpunit tests
+
+test-coverage:
+	@coverage=$$(XDEBUG_MODE=coverage composer exec phpunit tests -- --coverage-text --coverage-clover coverage.xml | grep 'Lines:' | sed -E 's/.*Lines:[[:space:]]*([0-9.]+)%.*/\1/'); \
+	echo "Coverage: $$coverage%"; \
+	echo "Minimum required: $(COVERAGE_MIN)%"; \
+	if [ "$$(echo "$$coverage < $(COVERAGE_MIN)" | bc -l)" -eq 1 ]; then \
+		echo "Coverage is too low!"; \
+		exit 1; \
+	fi
---
+++ tests/DiffetTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Hexlet\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Hexlet\Code\genDiff;
+
+class DiffetTest extends TestCase
+{
+    public function testSameValues(): void
+    {
+        $file = __DIR__ . "/fixtures/same.json";
+        $diff = genDiff($file, $file);
+        $this->assertEquals("  host: hexlet.io", $diff);
+    }
+
+    public function testDifferentValues(): void
+    {
+        $file1 = __DIR__ . "/fixtures/changed_first.json";
+        $file2 = __DIR__ . "/fixtures/changed_second.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals("- timeout: 50\n+ timeout: 20", $diff);
+    }
+
+    public function testOnlyInFirst(): void
+    {
+        $file1 = __DIR__ . "/fixtures/only_one.json";
+        $file2 = __DIR__ . "/fixtures/same.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals("  host: hexlet.io\n- proxy: 123.234.53.22", $diff);
+    }
+
+    public function testOnlyInSecond(): void
+    {
+        $file1 = __DIR__ . "/fixtures/same.json";
+        $file2 = __DIR__ . "/fixtures/only_one.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals("  host: hexlet.io\n+ proxy: 123.234.53.22", $diff);
+    }
+    public function testEmptyFiles(): void
+    {
+        $file = __DIR__ . "/fixtures/empty.json";
+        $diff = genDiff($file, $file);
+        $this->assertEmpty($diff);
+    }
+
+    public function testFirstEmpty(): void
+    {
+        $file1 = __DIR__ . "/fixtures/empty.json";
+        $file2 = __DIR__ . "/fixtures/same.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals("+ host: hexlet.io", $diff);
+    }
+
+    public function testSecondEmpty(): void
+    {
+        $file1 = __DIR__ . "/fixtures/same.json";
+        $file2 = __DIR__ . "/fixtures/empty.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals("- host: hexlet.io", $diff);
+    }
+
+    public function testAll(): void
+    {
+        $file1 = __DIR__ . "/fixtures/all_first.json";
+        $file2 = __DIR__ . "/fixtures/all_second.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals(
+            "- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true",
+            $diff
+        );
+    }
+
+    public function testTypes(): void
+    {
+        $file1 = __DIR__ . "/fixtures/to_string_first.json";
+        $file2 = __DIR__ . "/fixtures/to_string_second.json";
+        $diff = genDiff($file1, $file2);
+        $this->assertEquals(
+            "- enabled: false\n+ enabled: true\n- retries: 3\n+ retries: 3",
+            $diff
+        );
+    }
+}
